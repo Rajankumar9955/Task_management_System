61e3989065035a3d8f71107b005709c955e2b92a
Rajan <krajan92946@gmail.com>
2025-03-08 23:42:25 +0530

    Footer name changes

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -191,7 +191,7 @@ const Footer=()=>{
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         © 2020 Copyright:
         <a className='text-white' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
+          Rajan Kumar
         </a>
       </div>
     </MDBFooter>
